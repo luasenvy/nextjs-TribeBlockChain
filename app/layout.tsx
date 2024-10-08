0e703f3bd8dea65050640a4269dd27c53bff1d80
luasenvy <luas.envy@gmail.com>
2024-10-08 22:19:06 +0900

    fix: footer social links

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,13 @@ import { Inter } from "next/font/google";
 export const metadata = {
   title: "Astro TribeBlockChain",
   description: "Astro TribeBlockChain Template",
+  icons: [
+    {
+      rel: "icon",
+      type: "image/svg+xml",
+      url: "/favicon.svg",
+    },
+  ],
 };
 
 const inter = Inter({ subsets: ["latin"], display: "swap" });
